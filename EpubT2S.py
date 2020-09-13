72f838e4dccb154e16c04b1c87ee92d9ec5aca95
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2020-09-13 19:12:39 +0800

    Update EpubT2S.py

--- EpubT2S.py
+++ EpubT2S.py
@@ -1,69 +1,76 @@
-import os, shutil
-import zipfile
-import tkinter as tk
+import os, shutil , _thread, zipfile
+from tkinter import *
 from tkinter import filedialog
 from opencc import OpenCC
 
-root = tk.Tk()
-root.withdraw()
+def epubt2s():
+    root = Tk()
+    root.withdraw()
 
-# 获取桌面路径
-desktop_path = os.path.join(os.path.expanduser("~"), 'Desktop')
+    # 获取桌面路径
+    desktop_path = os.path.join(os.path.expanduser("~"), 'Desktop')
 
-# 获取当前文件夹路径
-now_path = os.getcwd()
+    # 获取当前文件夹路径
+    current_path = os.getcwd()
 
-# 选择文件
-file = filedialog.askopenfilename(
-    title = '选择Epub文件',
-    filetype = [('Epub电子书', '*.epub')],
-    initialdir = desktop_path
-    )
+    # 选择文件
+    file = filedialog.askopenfilename(
+        title = '选择Epub文件',
+        filetype = [('Epub电子书', '*.epub')],
+        initialdir = desktop_path
+        )
 
-# Epub解压文件夹
-foldername = os.path.basename(file)
+    # opencc t2s
+    cc = OpenCC('t2s')
 
-# 解压Epub到同名文件夹
-unzipf = zipfile.ZipFile(file, 'r')
-unzipf.extractall(foldername)
-unzipf.close()
+    # Epub解压文件夹
+    # foldername是epub名，folder是文件夹名
+    foldername = os.path.basename(file)
+    folder = os.path.splitext(foldername)[0]
 
-# 遍历解压的Epub文件夹里的所有文件
-def get_zip_file(input_path, result):
-    files = os.listdir(input_path)
-    for file in files:
-        if os.path.isdir(input_path + '/' + file):
-            get_zip_file(input_path + '/' + file, result)
-        else:
-            result.append(input_path + '/' + file)
+    s_foldername = cc.convert(foldername)
+    s_folder = cc.convert(folder)
 
-filelist = []
-get_zip_file(foldername, filelist)
+    # 解压Epub到同名文件夹
+    unzipf = zipfile.ZipFile(file, 'r')
+    unzipf.extractall(s_folder)
+    unzipf.close()
 
-# OpenCC处理文件
-convert_list = []
-for path in filelist:
-    suffix = os.path.splitext(path)[1]
-    text_suffix_list = ['.opf', '.ncx', '.xhtml']
-    for i in text_suffix_list:
-        if(i == suffix):
-            convert_list.append(path)
+    # 遍历解压的Epub文件夹里的所有文件
+    def get_zip_file(input_path, result):
+        files = os.listdir(input_path)
+        for file in files:
+            if os.path.isdir(input_path + '/' + file):
+                get_zip_file(input_path + '/' + file, result)
+            else:
+                result.append(input_path + '/' + file)
 
-# opencc t2s
-cc = OpenCC('t2s')
+    filelist = []
+    get_zip_file(s_folder, filelist)
 
-for path in convert_list:
-    cmd = 'python -m opencc -c t2s -i ' + path + ' -o ' + path
-    os.system(cmd)
+    # OpenCC处理文件
+    convert_list = []
+    for path in filelist:
+        suffix = os.path.splitext(path)[1]
+        text_suffix_list = ['.opf', '.ncx', '.xhtml']
+        for i in text_suffix_list:
+            if(i == suffix):
+                convert_list.append(path)
 
-s_foldername = cc.convert(foldername)
+    for path in convert_list:
+        CCcmd = 'python -m opencc -c t2s -i ' + path + ' -o ' + path
+        os.system(CCcmd)
 
-# 压缩文件夹为Epub
-zipf = zipfile.ZipFile(now_path + '/' + s_foldername, 'w', zipfile.ZIP_DEFLATED)
+    # 打包文件夹为Epub
+    zipf = zipfile.ZipFile(current_path + '/' + s_foldername, 'w', zipfile.ZIP_DEFLATED)
 
-for file in filelist:
-    zipf.write(file)
+    for file in filelist:
+        zipf.write(file)
 
-zipf.close()
+    zipf.close()
+    
+    shutil.rmtree(s_folder)
 
-shutil.rmtree(foldername)
+_thread.start_new_thread(epubt2s, ())
+
+input()
